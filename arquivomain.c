37f49ddaf8a0f48493483ea3f7c7a8623cbf0039
tato <tatofelicio2006@gmail.com>
2025-09-09 13:45:45 -0300

    inclusão de prototipos até 11

--- arquivomain.c
+++ arquivomain.c
@@ -36,3 +36,25 @@ typedef struct td_compromisso{
     td_data data;
     char descricao[300];
 } td_compromisso;
+
+void cadastra_aluno(td_aluno [], int);
+
+void cadastra_disciplina(td_disciplina [], int);
+
+void cadastra_matricula(td_matricula [], int, td_aluno [],  int,td_disciplina [], int);
+
+int verifica_data(td_data *);
+
+int verifica_horario(td_hora *);
+
+void cadastra_compromisso(td_compromisso *, int *, td_aluno *, int);
+
+void imprime_vetor_de_alunos(td_aluno [], int);
+
+int encontra_aluno(td_aluno *, int, int);
+
+int encontra_disciplina(td_lista_disciplinas *, int, int);
+
+int procura_data(td_compromisso *, int, td_data *, int);
+
+int procura_horario(td_compromisso *, int, td_hora *, td_data *, int);

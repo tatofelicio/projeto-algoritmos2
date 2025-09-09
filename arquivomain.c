2db3dfa156308e5d02bdc3278ca96e1d59e963b1
MuriloSilvaEng <contato.mdahmer@gmail.com>
2025-09-09 14:02:51 -0300

    Inclusão dos protótipos restantes

--- arquivomain.c
+++ arquivomain.c
@@ -58,3 +58,31 @@ int encontra_disciplina(td_lista_disciplinas *, int, int);
 int procura_data(td_compromisso *, int, td_data *, int);
 
 int procura_horario(td_compromisso *, int, td_hora *, td_data *, int);
+
+int verifica_email(char []);
+
+td_aluno* cria_vetor_alunos(int tamanho_inicial);
+td_disciplina* cria_vetor_disciplinas(int tamanho_inicial);
+td_matricula* cria_vetor_matriculas(int tamanho_inicial);
+td_compromisso* cria_vetor_compromissos(int tamanho_inicial);
+
+td_aluno* realoca_vetor_alunos(td_aluno *vetor, int novo_tamanho);
+td_disciplina* realoca_vetor_disciplinas(td_disciplina *vetor, int novo_tamanho);
+td_matricula* realoca_vetor_matriculas(td_matricula *vetor, int novo_tamanho);
+td_compromisso* realoca_vetor_compromissos(td_compromisso *vetor, int novo_tamanho);
+
+void imprime_relatorio_alunos(td_aluno *, int);
+
+void imprime_relatorio_compromissos(td_compromisso *, int);
+
+int compara_aluno_ra(const void *a, const void *b);
+
+int compara_compromisso_data_hora_ra(const void *a, const void *b);
+
+int compara_compromisso_hora_ra(const void *a, const void *b);
+
+int compara_compromisso_ra_data_hora(const void *a, const void *b);
+
+void le_horario(td_hora *);
+
+void le_data(td_data *);

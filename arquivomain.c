8f9b163aed079fa69b58d14326fb3d4ec0541d8a
MuriloSilvaEng <contato.mdahmer@gmail.com>
2025-09-11 11:03:13 -0300

    Organizando sintaxe do código

--- arquivomain.c
+++ arquivomain.c
@@ -1,88 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct td_data{
+typedef struct{
     int dia;
     int mes;
     int ano;
-} td_data;
+} stData;
 
-typedef struct td_hora{
+typedef struct{
     int hora;
     int min;
-} td_hora;
+} stHora;
 
-typedef struct td_disciplina{
-    int codigo_dis;
+typedef struct{
+    int codigoDis;
     char disciplina[100];
-} td_disciplina;
+} stDisciplina;
 
-typedef struct td_aluno{
+typedef struct{
     int ra;
     char nome[100];
     char email[100];
-} td_aluno;
+} stAluno;
 
-typedef struct td_matricula{
+typedef struct{
     int ra;
-    int codigo_dis;
-    td_data data_matricula;
+    int codigoDis;
+    stData dataMatricula;
     float notas[4];
-} td_matricula;
+} stMatricula;
 
-typedef struct td_compromisso{
-    td_aluno aluno;
-    td_hora horario;
-    td_data data;
+typedef struct{
+    stAluno aluno;
+    stHora horario;
+    stData data;
     char descricao[300];
-} td_compromisso;
+} stCompromisso;
 
-void cadastra_aluno(td_aluno [], int);
+// Funções de cadastro
+void cadastraAluno(stAluno [], int);
+void cadastraDisciplina(stDisciplina [], int);
+void cadastraMatricula(stMatricula [], int, stAluno [],  int, stDisciplina [], int);
+void cadastraCompromisso(stCompromisso *, int *, stAluno *, int);
 
-void cadastra_disciplina(td_disciplina [], int);
+// Funções de validação
+int verificaData(stData *);
+int verificaHorario(stHora *);
+int verificaEmail(char []);
 
-void cadastra_matricula(td_matricula [], int, td_aluno [],  int,td_disciplina [], int);
+// Funções de busca
+int encontraAluno(stAluno *, int, int);
+int encontraDisciplina(stDisciplina *, int, int);
+int procuraData(stCompromisso *, int, stData *, int);
+int procuraHorario(stCompromisso *, int, stHora *, stData *, int);
 
-int verifica_data(td_data *);
+// Funções de impressão
+void imprimeVetorDeAlunos(stAluno [], int);
+void imprimeRelatorioAlunos(stAluno *, int);
+void imprimeRelatorioCompromissos(stCompromisso *, int);
 
-int verifica_horario(td_hora *);
+// Funções de alocação dinâmica
+stAluno* criaVetorAlunos(int tamanhoInicial);
+stDisciplina* criaVetorDisciplinas(int tamanhoInicial);
+stMatricula* criaVetorMatriculas(int tamanhoInicial);
+stCompromisso* criaVetorCompromissos(int tamanhoInicial);
 
-void cadastra_compromisso(td_compromisso *, int *, td_aluno *, int);
+stAluno* realocaVetorAlunos(stAluno *vetor, int novoTamanho);
+stDisciplina* realocaVetorDisciplinas(stDisciplina *vetor, int novoTamanho);
+stMatricula* realocaVetorMatriculas(stMatricula *vetor, int novoTamanho);
+stCompromisso* realocaVetorCompromissos(stCompromisso *vetor, int novoTamanho);
 
-void imprime_vetor_de_alunos(td_aluno [], int);
+// Funções auxiliares para qsort
+int comparaAlunoRa(const void *a, const void *b);
+int comparaCompromissoDataHoraRa(const void *a, const void *b);
+int comparaCompromissoHoraRa(const void *a, const void *b);
+int comparaCompromissoRaDataHora(const void *a, const void *b);
 
-int encontra_aluno(td_aluno *, int, int);
-
-int encontra_disciplina(td_lista_disciplinas *, int, int);
-
-int procura_data(td_compromisso *, int, td_data *, int);
-
-int procura_horario(td_compromisso *, int, td_hora *, td_data *, int);
-
-int verifica_email(char []);
-
-td_aluno* cria_vetor_alunos(int tamanho_inicial);
-td_disciplina* cria_vetor_disciplinas(int tamanho_inicial);
-td_matricula* cria_vetor_matriculas(int tamanho_inicial);
-td_compromisso* cria_vetor_compromissos(int tamanho_inicial);
-
-td_aluno* realoca_vetor_alunos(td_aluno *vetor, int novo_tamanho);
-td_disciplina* realoca_vetor_disciplinas(td_disciplina *vetor, int novo_tamanho);
-td_matricula* realoca_vetor_matriculas(td_matricula *vetor, int novo_tamanho);
-td_compromisso* realoca_vetor_compromissos(td_compromisso *vetor, int novo_tamanho);
-
-void imprime_relatorio_alunos(td_aluno *, int);
-
-void imprime_relatorio_compromissos(td_compromisso *, int);
-
-int compara_aluno_ra(const void *a, const void *b);
-
-int compara_compromisso_data_hora_ra(const void *a, const void *b);
-
-int compara_compromisso_hora_ra(const void *a, const void *b);
-
-int compara_compromisso_ra_data_hora(const void *a, const void *b);
-
-void le_horario(td_hora *);
-
-void le_data(td_data *);
+// Funções opcionais
+void leHorario(stHora *);
+void leData(stData *);
